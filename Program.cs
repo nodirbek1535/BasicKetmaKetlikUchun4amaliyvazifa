547085d6e83ef584af4293686739e3d92bb4b549
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-03-30 10:16:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//1-vazifa. Katta harf tekshiruvi (Char & Bool)
+char harf;
+Console.WriteLine("Tekshirmoqchi bolgan harfni kiriting: ");
+harf = Convert.ToChar(Console.ReadLine());
+if (char.IsUpper(harf))
+    Console.WriteLine("Ha");
+else Console.WriteLine("Yo'q");
+
+
+//2-vazifa. Javobni aniqlash
+Console.WriteLine("Dasturlash sizga yoqadimi? (H/Y)");
+char javob = Convert.ToChar(Console.ReadLine());
+if (javob == 'H')
+    Console.WriteLine("Ajoyib!");
+else
+    Console.WriteLine("Afsus!");
+
+//3-vazifa. Ismning birinchi harfini chiqarish
+Console.WriteLine("Ismingizni kiriting: ");
+string ism = Console.ReadLine();
+Console.WriteLine(ism[0]);
